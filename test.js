9d967b0c29d6bfda50318b84d6833ff7c1dab09b
YujinOk <72746448+YujinOk@users.noreply.github.com>
2021-09-08 20:52:28 +1000

    Created variable that holds an object but it does not work, can you please have a look at it? and also added html file

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 // Add your variable under here, in line 2
+const Yujin = {
+  firstName: "Yujin",
+  lastName: "Ok",
+  eyeColor: "Dark Brown",
+  hobbies: "coding",
+  Ocupation: "lab tech",
+  hairColor: "black",
+};
+
+return `${this.firstName} has ${this.eyeColor} eyes`;
